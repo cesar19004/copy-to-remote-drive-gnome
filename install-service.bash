faf14b0103559e6e304cb3b25b8d7f1943f302fc
César Quintana <43949299+cesar19004@users.noreply.github.com>
2025-02-18 04:49:19 -0600

    Fix "Operation not supported"
    Create a service as user to use `gio` and access its remote storage locations. This also eliminates the need for root privileges.

--- install-service.bash
+++ install-service.bash
@@ -1,14 +1,20 @@
 #!/bin/bash
 
+SERVICEPATH="$HOME/.config/systemd/user"
+SCRIPTPATH="$HOME/.bin"
+
+# Create a path to store the script if it does not exist.
+mkdir -p $SCRIPTPATH
+
 #Append backup_files.bash without shebang to .env
-{ cat .env ; echo''; tail -n +2 backup_files.bash; } > /usr/local/bin/backup_files.bash || exit 1
+{ cat .env ; echo''; tail -n +2 backup_files.bash; } > "$SCRIPTPATH/backup_files.bash" || exit 1
 
-chmod +x /usr/local/bin/backup_files.bash
+chmod +x "$SCRIPTPATH/backup_files.bash"
 
-cp service/backup_files.service /etc/systemd/system/backup_files.service
+cp service/backup_files.service $SERVICEPATH/backup_files.service
 
-cp service/backup_files.timer /etc/systemd/system/backup_files.timer
+cp service/backup_files.timer $SERVICEPATH/backup_files.timer
 
-systemctl daemon-reload
-systemctl enable backup_files.timer
-systemctl start backup_files.timer
+systemctl --user daemon-reload
+systemctl --user enable backup_files.timer
+systemctl --user start backup_files.timer
